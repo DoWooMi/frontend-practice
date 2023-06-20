7980eca244a39d6a4cd3be1e549e5770753cfb4c
DoWooMi <kjk2875@gmail.com>
2023-06-20 21:56:02 +0900

    React Router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
+import Links from "./components/Links";
+import NavLinks from "./components/NavLinks";
+import Login from "./pages/Login";
+
+const isLogin = false;
 // react-router-dom version6이상부터 <Route>를 <Routes>안에 감싸줘야함
 function App() {
   return (
     <BrowserRouter>
+      <Links />
+      <NavLinks />
       <Routes>
+        <Route
+          path="/login"
+          element={isLogin ? <Navigate to="/" /> : <Login />}
+        />
         <Route path="/profile/:id/*" element={<Profile />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/about" element={<About />} />
